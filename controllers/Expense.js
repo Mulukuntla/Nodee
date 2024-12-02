282eb74af08897e2c3c8fd47333f7807bbae29f0
Mulukuntla <yours.saisaketh@gmail.com>
2024-12-02 13:02:55 +0530

    git update

--- controllers/Expense.js
+++ controllers/Expense.js
@@ -1,4 +1,5 @@
-const Expense= require("../models/Expense")
+//const Expense= require("../models/Expense")
+const User= require("../models/Expense")
 const ExpenseTracker= require("../models/ExpenseTracker")
 const Income= require("../models/income")
 const bcrypt = require('bcrypt');
@@ -20,25 +21,33 @@ function isstringinvalid(string){
 }
 
 const signup= async (req,res,next) =>{
-  const t=await sequelize.transaction()
+  
   try{
     const name=req.body.name;
     const email=req.body.email;
     const password=req.body.password;
     console.log(name,email,password)
+    const user=await User.findOne({where:{email}})
+    console.log("user------------>",user)
+    if(user){
+      
+      return res.status(400).json({message:"usage already exits",success:"false"})
+    }
     if(isstringinvalid(name) || isstringinvalid(email) || isstringinvalid(password)){
       console.log("Hi")
       return res.status(400).json({err:"Bad parameters - Something is missing"})
     }
     const saltrounds=10
     bcrypt.hash(password,saltrounds,async (err,hash)=>{
-      await Expense.create({userName:name,email:email,password:hash},{transaction:t})
+      if (err) {
+        return res.status(500).json({ success: false, message: "Error hashing password." });
+      }
+      await User.create({userName:name,email:email,password:hash})
       res.status(201).json({message:"Successfully created a new user"})
-      await t.commit()
+      
     })
   }  
   catch(err){
-    await t.rollback()
     res.status(500).json(err);
   }
 }
@@ -56,7 +65,7 @@ const signin= async (req,res,next) =>{
     if( isstringinvalid(email) || isstringinvalid(password)){
      return res.status(400).json({message:"email or password is missing",success:false,})
     }
-    const user=await Expense.findAll({ where: { email} })
+    const user=await User.findAll({ where: { email} })
     if(user.length>0){
       bcrypt.compare(password,user[0].password,(err,result)=>{
         if(err){
@@ -82,9 +91,9 @@ const signin= async (req,res,next) =>{
 }
 
 const download= async (req,res,next) =>{
-  const t=await sequelize.transaction()
+  
   try{
-    const user=await Expense.findOne({where:{id:req.user.id}})
+    const user=await User.findOne({where:{id:req.user.id}})
     if(user.ispremiumuser===null){
       return res.status(401).json({success:false,message:"not a premium user"})
     }
@@ -106,19 +115,20 @@ const download= async (req,res,next) =>{
     console.log(expenses)
     console.log(stringifiedExpenses)
     const fileUrl=await S3service.uploadToS3(stringifiedExpenses,filename)
-    const data=await allDownloads.create({date:new Date(),links:fileUrl,userId:req.user.id},{transaction:t})
+    const data=await allDownloads.create({date:new Date(),links:fileUrl,userId:req.user.id})
+  
     res.status(200).json({fileUrl,success:true})
-    await t.commit()
+    
   }
   catch(err){
-    await t.rollback()
+    
     console.log(err)
     res.status(500).json({fileUrl:"",success:false,err:err})
   }
 }
 const totaldownloads= async (req,res,next) =>{
   try{
-    const user=await Expense.findOne({where:{id:req.user.id}})
+    const user=await User.findOne({where:{id:req.user.id}})
     if(user.ispremiumuser===null){
       return res.status(401).json({success:false,message:"not a premium user"})
     }
@@ -133,7 +143,7 @@ const totaldownloads= async (req,res,next) =>{
 }
 const ispremiumuser= async (req,res,next) =>{
   try{
-    const user=await Expense.findOne({where:{id:req.user.id}})
+    const user=await User.findOne({where:{id:req.user.id}})
     if(user.ispremiumuser===null){
       return res.status(401).json({success:false,message:"not a premium user"})
     }
